f96cd50d80a23dc288b11ec6af5382fbb3c31a1a
woals7480 <94383932+woals7480@users.noreply.github.com>
2024-06-16 22:09:31 +0900

    context api 사용

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,5 +1,30 @@
+import styles from "./home.module.css";
+import Tab from "@/app/(afterLogin)/home/_components/Tab";
+import TabProvider from "@/app/(afterLogin)/home/_components/TabProvider";
+import PostForm from "@/app/(afterLogin)/home/_component/PostForm";
+import Post from "@/app/(afterLogin)/_component/Post";
+
 const Home = () => {
-  return <main>메인 홈페이지</main>;
+  return (
+    <main className={styles.main}>
+      <TabProvider>
+        <Tab />
+        {/* <PostForm /> */}
+        {/* <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post /> */}
+      </TabProvider>
+    </main>
+  );
 };
 
 export default Home;
