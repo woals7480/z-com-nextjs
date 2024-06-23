7dc7fbdbcae53466daacaeeb755ca4cfcb1b56ff
woals7480 <94383932+woals7480@users.noreply.github.com>
2024-06-23 21:45:46 +0900

    status/[id] 페이지 & onClickCapture 사용

--- src/app/(afterLogin)/_components/Post.tsx
+++ src/app/(afterLogin)/_components/Post.tsx
@@ -4,12 +4,14 @@ import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import "dayjs/locale/ko";
 import ActionButtons from "@/app/(afterLogin)/_components/ActionButtons";
+import PostArticle from "@/app/(afterLogin)/_components/PostArticle";
 
 dayjs.locale("ko");
 dayjs.extend(relativeTime);
 
 export default function Post() {
   const target = {
+    postId: 1,
     User: {
       id: "elonmusk",
       nickname: "Elon Musk",
@@ -17,10 +19,10 @@ export default function Post() {
     },
     content: "클론코딩 라이브로 하니 너무 힘들어요 ㅠㅠ",
     createdAt: new Date(),
-    Images: [],
+    Images: [] as any[],
   };
   return (
-    <article className={styles.post}>
+    <PostArticle post={target}>
       <div className={styles.postWrapper}>
         <div className={styles.postUserSection}>
           <Link href={`/${target.User.id}`} className={styles.postUserImage}>
@@ -47,6 +49,6 @@ export default function Post() {
           <ActionButtons />
         </div>
       </div>
-    </article>
+    </PostArticle>
   );
 }
